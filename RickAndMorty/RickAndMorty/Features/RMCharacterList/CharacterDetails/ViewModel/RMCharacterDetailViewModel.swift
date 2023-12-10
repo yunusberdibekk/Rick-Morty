7c3698557612e2549dfbea774364cde5590d4207
Yunus Emre Berdibek <ye.berdibek@gmail.com>
2023-12-10 20:30:18 +0300

    CharacterInformationCell added.

--- RickAndMorty/RickAndMorty/Features/RMCharacterList/CharacterDetails/ViewModel/RMCharacterDetailViewModel.swift
+++ RickAndMorty/RickAndMorty/Features/RMCharacterList/CharacterDetails/ViewModel/RMCharacterDetailViewModel.swift
@@ -40,14 +40,14 @@ extension RMCharacterDetailViewModel {
         sections = [
             .photo(viewModel: .init(imageURL: URL(string: character.image))),
             .information(viewModels: [
-                .init(value: character.status.text, title: "Status"),
-                .init(value: character.gender.rawValue, title: "Gender"),
-                .init(value: character.type, title: "Type"),
-                .init(value: character.species, title: "Species"),
-                .init(value: character.origin.name, title: "Origin"),
-                .init(value: character.location.name, title: "Location"),
-                .init(value: character.created, title: "Created"),
-                .init(value: "\(character.episode.count)", title: "Total Episode"),
+                .init(type: .status, value: character.status.text),
+                .init(type: .gender, value: character.gender.rawValue),
+                .init(type: .type, value: character.type),
+                .init(type: .species, value: character.species),
+                .init(type: .origin, value: character.origin.name),
+                .init(type: .location, value: character.location.name),
+                .init(type: .created, value: character.created),
+                .init(type: .episodeCount, value: "\(character.episode.count)"),
             ]),
             .episodes(viewModels:
                 character.episode.compactMap {
--- RickAndMorty/RickAndMorty/Features/RMCharacterList/CharacterDetails/ViewModel/RMCharacterInformationCollectionViewCellViewModel.swift
+++ RickAndMorty/RickAndMorty/Features/RMCharacterList/CharacterDetails/ViewModel/RMCharacterInformationCollectionViewCellViewModel.swift
@@ -5,16 +5,116 @@
 //  Created by Yunus Emre Berdibek on 10.12.2023.
 //
 
-import Foundation
+import UIKit
 
 final class RMCharacterInformationCollectionViewCellViewModel {
     /// Properties
-    public let value: String
-    public let title: String
-
+    private let type: `Type`
+    private let value: String
+    
+    static var shortDateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    static var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSZ"
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    public var title: String {
+        type.displayTitle
+    }
+    
+    public var displayValue: String {
+        if value.isEmpty { return "None" }
+        if let date = Self.dateFormatter.date(from: value), type == .created {
+            return Self.shortDateFormatter.string(from: date)
+        }
+        return value
+    }
+    
+    public var iconImage: UIImage? {
+        type.iconImage
+    }
+    
+    public var tintColor: UIColor {
+        type.tintColor
+    }
+    
+    enum `Type`: String {
+        case status
+        case gender
+        case type
+        case species
+        case origin
+        case created
+        case location
+        case episodeCount
+        
+        var displayTitle: String {
+            switch self {
+            case .status, .gender, .type, .species, .origin, .created, .location:
+                return rawValue.uppercased()
+            case .episodeCount:
+                return "EPISODE COUNT"
+            }
+        }
+        
+        var iconImage: UIImage? {
+            switch self {
+            case .status:
+                return UIImage(systemName: "bell")
+            case .gender:
+                return UIImage(systemName: "bell")
+            case .type:
+                return UIImage(systemName: "bell")
+            case .species:
+                return UIImage(systemName: "bell")
+            case .origin:
+                return UIImage(systemName: "bell")
+            case .created:
+                return UIImage(systemName: "bell")
+            case .location:
+                return UIImage(systemName: "bell")
+            case .episodeCount:
+                return UIImage(systemName: "bell")
+            }
+        }
+        
+        var tintColor: UIColor {
+            switch self {
+            case .status:
+                return .systemBlue
+            case .gender:
+                return .systemRed
+            case .type:
+                return .systemPurple
+            case .species:
+                return .systemGreen
+            case .origin:
+                return .systemOrange
+            case .created:
+                return .systemPink
+            case .location:
+                return .systemYellow
+            case .episodeCount:
+                return .systemMint
+            }
+        }
+    }
+    
     /// Init
-    init(value: String, title: String) {
+    init(
+        type: Type,
+        value: String)
+    {
+        self.type = type
         self.value = value
-        self.title = title
     }
 }
